4783a23f07602424b22c498ae5596f72d4168f8d
Andrey Fedoseev <andrey.fedoseev@gmail.com>
2011-06-25 23:17:35 +0400

    Fix 404 when url contains no slug

--- apps/blog/views/entry.py
+++ apps/blog/views/entry.py
@@ -10,13 +10,25 @@ class Entry(BlogViewMixin, TemplateView):
 
     template_name = "blog/entry.html"
 
+    def get(self, request, *args, **kwargs):
+        id = int(kwargs["id"])
+        slug = kwargs.get("slug")
+        filters = dict(id=id, blog=self.blog)
+        if slug:
+            filters["slug"] = slug
+        entry = get_object_or_404(EntryModel, **filters)
+        if entry.slug and slug != entry.slug:
+            return redirect(entry, permanent=True)
+        return super(Entry, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         data = super(Entry, self).get_context_data(**kwargs)
         id = int(kwargs["id"])
         slug = kwargs.get("slug")
-        entry = get_object_or_404(EntryModel, blog=self.blog, id=id, slug=slug)
-        if entry.slug and slug != entry.slug:
-            return redirect(entry, permanent=True)
+        filters = dict(id=id, blog=self.blog)
+        if slug:
+            filters["slug"] = slug
+        entry = get_object_or_404(EntryModel, **filters)
         page_title = entry.title
 
         if self.is_author:
